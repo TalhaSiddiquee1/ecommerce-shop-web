66f96df0fda341449654b9e19e6a53911e5e7444
TalhaSiddiquee1 <sid.talha2022@gmail.com>
2026-02-25 10:47:06 +0600

    more updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,28 +3,76 @@ import Container from './Container';
 import FooterTop from './FooterTop';
 import Logo from './Logo';
 import SocialMedia from './SocialMedia';
+import { SubText, SubTitle } from './ui/text';
+import { categoriesData, quickLinksData } from '@/constants/data';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <Container>
         <FooterTop />
-        <div className=''>
-          <div className='space-y-4'>
+        <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="space-y-4">
             <Logo />
-            <p>
+            <SubText>
               Discover curated furniture collections at Shopcartyt, blending
               style, and comfort to elevate your living spaces.
-            </p>
+            </SubText>
             <SocialMedia
               className="text-shop-dark-color/60"
               iconClassName="border-shop-dark-color/60 hover:text-shop-light-green"
               tooltipClassName="bg-shop-dark-color text-white"
             />
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <SubTitle>Quick Links</SubTitle>
+            <ul className="space-y-3 mt-4">
+              {quickLinksData?.map((item) => (
+                <li key={item?.title}>
+                  <Link
+                    href={item?.href}
+                    className="hover:text-shop-light-green hoverEffect font-medium"
+                  >
+                    {item?.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <SubTitle>Categories</SubTitle>
+            <ul className="space-y-3 mt-4">
+              {categoriesData?.map((item) => (
+                <li key={item?.title}>
+                  <Link
+                    href={`/category/${item?.href}`}
+                    className="hover:text-shop-light-green hoverEffect font-medium"
+                  >
+                    {item?.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='space-y-4'>
+            <SubTitle>Newsletter</SubTitle>
+            <SubText>
+              Subscribe to our newsletter to get updates on our latest offers and
+              products.
+            </SubText>
+            <form className='space-y-3'>
+              <Input placeholder="Enter your email" type='email' required />
+              <Button className='w-full'>Subscribe</Button>
+            </form>
+          </div>
+        </div>
+        <div className='py-6 border-t text-center text-sm text-gray-600'>
+          <div>
+              &copy; {new Date().getFullYear()} <Logo className='text-sm' />. All rights reserved.
+          </div>
         </div>
       </Container>
     </footer>
--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 const Title = ({
   children,
   className,
@@ -6,7 +6,45 @@ const Title = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  return <h2 className={cn("text-3xl font-bold text-shop-dark-green capitalize tracking-wider font-sans", className)}>{children}</h2>;
+  return (
+    <h2
+      className={cn(
+        'text-3xl font-bold text-shop-dark-green capitalize tracking-wider font-sans',
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
 };
 
-export { Title };
+const SubTitle = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h2
+      className={cn(
+        'font-semibold text-gray-900 font-sans',
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+};
+
+const SubText = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <p className={cn('text-gray-600 text-sm', className)}>{children}</p>;
+};
+
+export { Title, SubTitle, SubText };
