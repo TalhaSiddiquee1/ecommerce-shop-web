3b8861f15c8ed6dbfdc9c1d348e0c3a7a96b28ab
TalhaSiddiquee1 <sid.talha2022@gmail.com>
2026-03-01 07:51:18 +0600

    more update

---
+++ components/PriceView.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PriceFormatter from './PriceFormater';
+
+interface Props {
+    price?: number | undefined;
+    discount?: number | undefined;
+    className?: string;
+    }
+
+const PriceView = ({ price, discount, className }: Props) => {
+  return (
+        <div className='flex items-center gap-2'>
+                <PriceFormatter amount={price} className='text-shop-btn-dark-green'/>
+                {price && discount && <PriceFormatter amount={price + (discount*price)/100} className='line-through font-normal text-shop-light-text' />}
+        </div>
+  )
+}
+
+export default PriceView
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,13 @@
 import { urlFor } from '@/sanity/lib/image';
-import { Flame } from 'lucide-react';
-import { p } from 'motion/react-client';
+import { Flame, StarIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import AddToWishlistButton from './AddToWishlistButton';
+import { Title } from './ui/text';
+import { Product } from '@/sanity.types';
+import PriceView from './PriceView';
+import AddToCartButton from './AddToCartButton';
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -16,8 +20,10 @@ const ProductCard = ({ product }: { product: Product }) => {
             loading="lazy"
             width={700}
             height={700}
+            className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop-light-bg hoverEffect ${product?.stock !== 0 ? 'group-hover:scale-105' : 'opacity-50'}`}
           />
         )}
+        <AddToWishlistButton product={product} className="" />
         {product?.status === 'sale' && (
           <p className="absolute top-2 left-2 z-10 text-xs border border-shop-dark-color/50 px-2 rounded-full group-hover:border-shop-light-green group-hover:text-shop-light-green hoverEffect">
             Sale!
@@ -41,7 +47,47 @@ const ProductCard = ({ product }: { product: Product }) => {
           </Link>
         )}
       </div>
-      <div className="p-3">Product details</div>
+      <div className="p-3">
+        {product?.categories?.[0] && (
+          <p className="uppercase line-clamp-1 text-xs text-shop-light-text">
+            {product?.categories?.map((cat) => cat).join(', ')}
+          </p>
+        )}
+        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-0.5">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <StarIcon
+                size={12}
+                key={index}
+                className={
+                  index < 4
+                    ? 'text-shop-lighter-green'
+                    : 'text-shop-lighter-text'
+                }
+                fill={index < 4 ? '#93d991' : '#ababab'}
+              />
+            ))}
+          </div>
+          <p className="text-shop-light-text text-xs tracking-wide">
+            5 Reviews
+          </p>
+        </div>
+        <div className="flex items-center gap-2.5">
+          <p className="font-medium">In Stock</p>
+          <p
+            className={`${product?.stock === 0 ? 'text-red-600' : 'text-shop-light-green/80 font-semibold'}`}
+          >
+            {(product?.stock as number) > 0 ? product?.stock : 'Out of Stock'}
+          </p>
+        </div>
+        <PriceView
+            price={product?.price}
+            discount={product?.discount}
+            className="text-sm"
+        />
+        <AddToCartButton product={product} className='w-36 rounded-full'/>
+      </div>
     </div>
   );
 };
