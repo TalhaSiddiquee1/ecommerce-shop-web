856e7a1f3bb8181b3129ec85020d146e8153ea9b
TalhaSiddiquee1 <sid.talha2022@gmail.com>
2026-03-06 10:35:39 +0600

    updates

--- sanity.types.ts
+++ sanity.types.ts
@@ -483,11 +483,52 @@ export type LATEST_BLOG_QUERY_RESULT = Array<{
   body?: BlockContent;
 }>;
 
+// Source: sanity/queries/query.ts
+// Variable: DEAL_PRODUCTS
+// Query: *[_type == 'product' && status == 'hot'] | order(name asc){    ...,"categories": categories[]->title  }
+export type DEAL_PRODUCTS_RESULT = Array<{
+  _id: string;
+  _type: 'product';
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  images?: Array<{
+    asset?: {
+      _ref: string;
+      _type: 'reference';
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: 'sanity.imageAsset';
+    };
+    media?: unknown;
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: 'image';
+    _key: string;
+  }>;
+  description?: string;
+  price?: number;
+  discount?: number;
+  categories: Array<string | null> | null;
+  stock?: number;
+  brand?: {
+    _ref: string;
+    _type: 'reference';
+    _weak?: boolean;
+    [internalGroqTypeReferenceTo]?: 'brand';
+  };
+  status: 'hot';
+  variant?: 'appliances' | 'gadget' | 'others' | 'refrigerators';
+  isFeatured?: boolean;
+}>;
+
 // Query TypeMap
 import '@sanity/client';
 declare module '@sanity/client' {
   interface SanityQueries {
     '\n  *[_type == "brand"] | order(title asc){\n    _id,\n    title,\n    slug,\n    "imageUrl": coalesce(image.asset->url, logo.asset->url),\n    "mimeType": coalesce(image.asset->mimeType, logo.asset->mimeType)\n  }\n': BRANDS_QUERY_RESULT;
     " *[_type == 'blog' && isLatest == true]|order(name asc){\n      ...,\n      blogcategories[]->{\n      title\n    }\n    }": LATEST_BLOG_QUERY_RESULT;
+    "*[_type == 'product' && status == 'hot'] | order(name asc){\n    ...,\"categories\": categories[]->title\n  }": DEAL_PRODUCTS_RESULT;
   }
 }
