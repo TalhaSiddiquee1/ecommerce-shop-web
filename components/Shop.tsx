583705e357d7a82defdb1cb82a643144eeb4f0ba
TalhaSiddiquee1 <sid.talha2022@gmail.com>
2026-03-10 11:05:40 +0600

    updates

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -1,5 +1,12 @@
-import { BRANDS_QUERYResult, Category } from '@/sanity.types';
-import React from 'react'
+'use client'
+import { BRANDS_QUERYResult, Category, Product } from '@/sanity.types';
+import React, { useState } from 'react'
+import Container from './Container';
+import { Title } from './ui/text';
+import CategoryList from './shop/CategoryList';
+import PriceList from './shop/PriceList';
+import BrandList from './shop/BrandList';
+import { useSearchParams } from 'next/navigation';
 
 interface Props {
   categories: Category[];
@@ -7,8 +14,37 @@ interface Props {
 }
 
 const Shop = ({ categories, brands }: Props) => {
+  const searchParams = useSearchParams();
+  const brandParams = searchParams?.get("brand");
+  const categoryParams = searchParams?.get("category");
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(
+    categoryParams || null
+  );
+  const [selectedBrand, setSelectedBrand] = useState<string | null>(
+    brandParams || null
+  );
+  const [selectedPrice, setSelectedPrice] = useState<string | null>(null);
   return (
-    <div>Shop</div>
+    <div className='border-t'>
+        <Container className='mt-5'>
+            <div className='sticky top-0 z-10 mb-5'>
+                <div className='flex items-center justify-between'>
+                    <Title className='text-lg uppercase tracking-wide'>Get the products as your needs</Title>
+                    <button className='text-shop-dark-green underline text-sm mt-2 font-medium hover:text-shop-orange hoverEffect'>Reset Filters</button>
+                </div>
+            </div>
+            <div className='flex flex-col md:flex-row gap-5 border-t border-t-shop-dark-green/50 '>
+                <div className='md:sticky md:top-20 md:self-start md:h-[calc(100vh-160px)] md:overflow-hidden md:min-w-64 pb-5 md:border-r border-r-shop-btn-dark-green/50'>
+                <CategoryList/>
+                <BrandList/>
+                <PriceList/>
+                </div>
+                <div>Products</div>
+            </div>
+        </Container>
+    </div>
   )
 }
 
