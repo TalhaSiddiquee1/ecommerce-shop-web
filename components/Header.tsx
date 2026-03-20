71ca13840d0bcc0606058c2b575f34758a4acc41
TalhaSiddiquee1 <sid.talha2022@gmail.com>
2026-03-20 15:33:55 +0600

    completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,8 +22,8 @@ const Header = async () => {
   }
 
   return (
-    <header className="bg-white/70 py-5 sticky top-0 z-50 backdrop-blur-md">
-      <Container className="flex items-center justify-between text-shop-light-color">
+    <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
+      <Container className="flex items-center justify-between text-lightColor">
         <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
           <MobileMenu />
           <Logo />
@@ -33,14 +33,21 @@ const Header = async () => {
           <SearchBar />
           <CartIcon />
           <FavoriteButton />
+
+          {user && (
+            <Link
+              href={'/orders'}
+              className="group relative hover:text-shop_light_green hoverEffect"
+            >
+              <Logs />
+              <span className="absolute -top-1 -right-1 bg-shop_btn_dark_green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+                {orders?.length ? orders?.length : 0}
+              </span>
+            </Link>
+          )}
+
           <ClerkLoaded>
             <SignedIn>
-              <Link href="/orders" className='group relative hover:text-shop-light-green'>
-                <Logs />
-                <span className="absolute -top-1 -right-1 bg-shop-btn-dark-green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
-                  {orders?.length ? orders?.length : 0}
-                </span>
-              </Link>
               <UserButton />
             </SignedIn>
             {!user && <SignIn />}
